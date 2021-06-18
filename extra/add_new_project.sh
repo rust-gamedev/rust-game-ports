9ecabc04082ac34fb0f6f7003fbb4306edbab50f
Saverio Miroddi <saverio.pub2@gmail.com>
2021-06-18 19:38:10 +0200

    add_new_project: Add support for project name suffix

--- extra/add_new_project.sh
+++ extra/add_new_project.sh
@@ -7,15 +7,18 @@ set -o errtrace
 shopt -s inherit_errexit
 
 c_reference_project=boing-ggez
-v_project_name=
+v_original_project=
+v_new_project=
 
 function decode_cmdline_args {
   if [[ $# -ne 1 || $1 == "-h" || $1 == "--help" ]]; then
-    echo "Usage: $(basename "$0") <project_name>"
+    echo "Usage: $(basename "$0") <project_name-suffix>"
+    echo "The suffix is for the new project, and it's optional; it supports dashes."
     exit 0
   fi
 
-  v_project_name=$1
+  v_original_project=${1%%-*}
+  v_new_project=$1
 }
 
 function move_to_root_dir {
@@ -23,26 +26,26 @@ function move_to_root_dir {
 }
 
 function check_project_name {
-  if [[ ! -d original_code/$v_project_name-master ]]; then
+  if [[ ! -d original_code/$v_original_project-master ]]; then
     >&2 echo 'Project original code not found!'
     exit 1
   fi
 }
 
 function add_project {
-  perl -i -0777 -pe "s/]$/  \"$v_project_name\",\n]/" Cargo.toml
-  cargo new "$v_project_name"
-  rsync -av --relative "$c_reference_project/./Cargo.toml" "$c_reference_project/./rust-toolchain" "$c_reference_project/./.cargo" "$v_project_name/"
-  perl -i -pe "s/$c_reference_project/$v_project_name/" "$v_project_name/Cargo.toml"
-  git ca -m "Add ${v_project_name^} project"
+  perl -i -0777 -pe "s/]$/  \"$v_new_project\",\n]/" Cargo.toml
+  cargo new "$v_new_project"
+  rsync -av --relative "$c_reference_project/./Cargo.toml" "$c_reference_project/./rust-toolchain" "$c_reference_project/./.cargo" "$v_new_project/"
+  perl -i -pe "s/$c_reference_project/$v_new_project/" "$v_new_project/Cargo.toml"
+  git ca -m "Add ${v_new_project^} project"
 }
 
 function move_original_code {
-  mv "original_code/$v_project_name-master" "$v_project_name/resources"
-  cp "$v_project_name/resources/$v_project_name.py" "$v_project_name/src"
-  sed -i '1i #!/usr/bin/env python3' "$v_project_name/resources/$v_project_name.py"
-  chmod 755 "$v_project_name/resources/$v_project_name.py"
-  git ca -m "${v_project_name^}: Move original project inside the Rust project"
+  mv "original_code/$v_original_project-master" "$v_new_project/resources"
+  cp "$v_original_project/resources/$v_original_project.py" "$v_new_project/src"
+  sed -i '1i #!/usr/bin/env python3' "$v_new_project/resources/$v_original_project.py"
+  chmod 755 "$v_new_project/resources/$v_original_project.py"
+  git ca -m "${v_new_project^}: Move original project inside the Rust project"
 }
 
 decode_cmdline_args "$@"
