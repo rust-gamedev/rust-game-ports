775b32e6224cc217ff93125a6fa3b663e44db085
Saverio Miroddi <saverio.pub2@gmail.com>
2021-06-30 21:34:10 +0200

    add_new_project.sh: Don't move the original code, if already done

--- extra/add_new_project.sh
+++ extra/add_new_project.sh
@@ -13,9 +13,9 @@ Usage: $(basename "$0") <project_name-suffix>
 Actions performed:
 
 - creates a new rust project, using \`$c_reference_project\` as template;
-- moves the resources/original code to the shared resources dir;
-- adds (another) of the python source file into the project;
-- symlinks the game resources into the project.
+- moves the original project to the shared resources dir;
+
+Note that this yields one copy of the source code under resources, and another copy on the new project.
 
 The suffix is for the new project, and it's optional; it supports dashes."
 
@@ -56,7 +56,7 @@ function move_to_root_dir {
 }
 
 function check_project_name {
-  if [[ ! -d original_code/$v_original_project-master ]]; then
+  if [[ ! -d original_code/$v_original_project-master && ! -d resources/$v_original_project ]]; then
     >&2 echo 'Project original code not found!'
     exit 1
   fi
@@ -64,19 +64,27 @@ function check_project_name {
 
 function add_project {
   cargo new "$v_new_project"
+
   rsync -av --relative "$c_reference_project/./Cargo.toml" "$c_reference_project/./rust-toolchain" "$c_reference_project/./.cargo" "$v_new_project/"
+
   perl -i -pe "s/$c_reference_project/$v_new_project/" "$v_new_project/Cargo.toml"
+
+  # At this stage, the file can be already under `resources/`, or not.
+  # It could also be under previous ports of the same game, so we can't just glob the way.
+  #
+  find resources original_code -name "$v_original_project.py" -exec cp -v {} "$v_new_project/src/" \;
+
+  # At this stage, this could be broken, but that's fine.
+  #
+  ln -s "../resources/$v_original_project" "$v_new_project/resources"
+
   git ca -m "Add ${v_new_project^} project"
 }
 
 function move_original_code {
-  cp "original_code/$v_original_project-master/$v_original_project.py" "$v_new_project/src"
-  ln -s "../resources/$v_original_project" "$v_new_project/resources"
-
   mv "original_code/$v_original_project-master" "resources/$v_original_project"
   sed -i '1i #!/usr/bin/env python3' "resources/$v_original_project/$v_original_project.py"
   chmod 755 "resources/$v_original_project/$v_original_project.py"
-
   git ca -m "${v_new_project^}: Move original project inside the shared resources dir"
 }
 
@@ -84,4 +92,6 @@ decode_cmdline_args "$@"
 move_to_root_dir
 check_project_name
 add_project
-move_original_code
+if [[ ! -d resources/$v_original_project ]]; then
+  move_original_code
+fi
