73838608ecd472eab32a2f7fd2b1e3c4ea650166
Saverio Miroddi <saverio.pub2@gmail.com>
2022-04-22 23:47:01 +0200

    rusty_roguelike-bevy: Add 06_EntitiesComponentsAndSystems_01_playerecs

---
+++ rusty_roguelike-bevy/06_EntitiesComponentsAndSystems_01_playerecs/src/systems/player_input.rs
@@ -0,0 +1,27 @@
+use crate::prelude::*;
+
+pub fn player_input(
+    mut query: Query<(&Player, &mut PointC)>, //(1) (2)
+    (map, key, mut camera): (Res<Map>, Res<VirtualKeyCodeR>, ResMut<crate::Camera>),
+) {
+    if let Some(key) = key.0 {
+        let delta = match key {
+            VirtualKeyCode::Left => Point::new(-1, 0),
+            VirtualKeyCode::Right => Point::new(1, 0),
+            VirtualKeyCode::Up => Point::new(0, -1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            _ => Point::new(0, 0),
+        };
+
+        if delta.x != 0 || delta.y != 0 {
+            for (_player, mut pos) in query.iter_mut() {
+                //(3)
+                let destination = pos.0 + delta;
+                if map.can_enter_tile(destination) {
+                    pos.0 = destination;
+                    camera.on_player_move(destination);
+                }
+            }
+        }
+    }
+}
