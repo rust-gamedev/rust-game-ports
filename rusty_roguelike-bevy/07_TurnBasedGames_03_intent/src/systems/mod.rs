9c6c20f2b84f30a59b28ab1138fc526471ae09f1
Saverio Miroddi <saverio.pub2@gmail.com>
2022-04-27 18:41:31 +0200

    Rusty Roguelike: (Cosmetic) forwardports to 07.03

--- rusty_roguelike-bevy/07_TurnBasedGames_03_intent/src/systems/mod.rs
+++ rusty_roguelike-bevy/07_TurnBasedGames_03_intent/src/systems/mod.rs
@@ -11,15 +11,40 @@ mod random_move;
 pub fn build_system_sets(app: &mut App) {
     use GameStep::*;
 
-    // Specifying the addition to CoreStage::Update is redundant, but it makes the structure clearer.
-
-    // In the port design, rendering is done at the beginning of each frame, rather than at the end.
-    // The difference is significant, architecturally; if rendering is performed at the end, it requires
-    // a flush (therefore, another step, in our design); by placing it at the beginning, it takes advantage
-    // of the flush performed at the end of the previous frame.
-    // In the overall steps sequence, the location is essentially the same, so there are no (bad) side
-    // effects.
+    // As of v0.7, Bevy has two fundamental deficiencies - it lacks on-demand flushing (it flushes only
+    // at the end of the frame), and states and stages can't be mixed.
+    // Without on-demand flushing, a system in a serialized sequence won't detect changes requested
+    // by the previous ones. A workaround is to execute only the set of systems associated to a single
+    // state for each frame (via `SystemSet::on_update(state)` filter), however, the state can't be
+    // updated during the CoreStage::Update stage (the default), otherwise, the next system set will
+    // kick off immediately, without waiting for the flush; in turn, a workaround could be to perform
+    // the state change in a separate stage, but this requires mixing states and stages, which is currently
+    // unsupported (likely, it doesn't work even if state filtering is performed by if/then/else tests
+    // inside each system).
     //
+    // In order to model this design, the `iyes_loopless` crate is required.
+    //
+    // Bevy's translation of Legion's schedulers and flushes are (iyes_loopless) states; besides supporting
+    // mixing stages and states, on state change, the frame is flushed (note that this will, in turn,
+    // cause issues with the Bevy events system).
+    //
+    // In order to translate the concept, we need to:
+    //
+    // - divide the systems into groups
+    // - create an enum for each group
+    // - run each set of systems during the corresponding state
+    // - add a state change system
+    // - perform rendering independently from the system set
+    //
+    // Compared to Legion, this is a non neglibile amount of boilerplate (and side effects), although
+    // the logic is straightforward.
+    // The state change system is also be required by Legion, but the amount of states is smaller.
+    //
+    // Finally, a side effect of iyes_loopless is that, since a state_change flushes the frame, and
+    // the library we use is set to run at a fixed amount of frames per second, the port is "slower",
+    // as it requires multiple frames for a full cycle. This is easy to improve, but this will be done
+    // separately.
+
     app.add_system_set(
         SystemSet::new()
             .with_system(map_render::map_render)
@@ -49,7 +74,7 @@ pub fn build_system_sets(app: &mut App) {
 
     app.add_system_set(
         ConditionSet::new()
-            .run_in_state(GenerateMonstersMovements)
+            .run_in_state(GenerateMonsterMoves)
             .with_system(random_move::random_move)
             .with_system(next_step::next_step)
             .into(),
