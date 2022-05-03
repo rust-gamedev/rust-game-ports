2165f1466e82babc965360d4b881f0ff07f069ae
Saverio Miroddi <saverio.pub2@gmail.com>
2022-05-03 22:57:33 +0200

    Rusty Roguelike: Port 13_InventoryAndPowerUps_01_potions_and_scrolls

---
+++ rusty_roguelike-bevy/13_InventoryAndPowerUps_01_potions_and_scrolls/src/spawner.rs
@@ -0,0 +1,96 @@
+use crate::prelude::*;
+
+pub fn spawn_player(world: &mut World, pos: Point) {
+    world.spawn().insert_bundle((
+        Player,
+        PointC(pos),
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('@'),
+        },
+        Health {
+            current: 10,
+            max: 10,
+        },
+        FieldOfView::new(8),
+    ));
+}
+
+pub fn spawn_entity(world: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let roll = rng.roll_dice(1, 6);
+    match roll {
+        1 => spawn_healing_potion(world, pos),
+        2 => spawn_magic_mapper(world, pos),
+        _ => spawn_monster(world, rng, pos),
+    }
+}
+
+pub fn spawn_monster(world: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
+
+    world.spawn().insert_bundle((
+        Enemy,
+        PointC(pos),
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph,
+        },
+        ChasingPlayer {},
+        Health {
+            current: hp,
+            max: hp,
+        },
+        Name(name),
+        FieldOfView::new(8),
+    ));
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
+}
+
+pub fn spawn_amulet_of_yala(world: &mut World, pos: Point) {
+    world.spawn().insert_bundle((
+        Item,
+        AmuletOfYala,
+        PointC(pos),
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('|'),
+        },
+        Name("Amulet of Yala".to_string()),
+    ));
+}
+
+pub fn spawn_healing_potion(world: &mut World, pos: Point) {
+    world.spawn().insert_bundle((
+        Item,
+        PointC(pos),
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('!'),
+        },
+        Name("Healing Potion".to_string()),
+        ProvidesHealing { amount: 6 },
+    ));
+}
+
+pub fn spawn_magic_mapper(world: &mut World, pos: Point) {
+    world.spawn().insert_bundle((
+        Item,
+        PointC(pos),
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: to_cp437('{'),
+        },
+        Name("Dungeon Map".to_string()),
+        ProvidesDungeonMap {},
+    ));
+}
