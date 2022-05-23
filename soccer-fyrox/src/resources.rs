b3277d365af0e9a830401b609c1ade3ccd14ee52
Saverio Miroddi <saverio.pub2@gmail.com>
2022-05-23 21:50:01 +0200

    Soccer-fyrox: Add automatic image size detection

--- soccer-fyrox/src/resources.rs
+++ soccer-fyrox/src/resources.rs
@@ -4,11 +4,9 @@ use fyrox::{
     core::futures::executor::block_on, engine::resource_manager::ResourceManager,
     resource::texture::Texture,
 };
+use imagesize::ImageSize;
 
-const IMAGE_PATHS: [(&str, u16, u16); 2] = [
-    ("resources/images/menu01.png", 800, 480),
-    ("resources/images/menu02.png", 800, 480),
-];
+const IMAGE_PATHS: [&str; 2] = ["resources/images/menu01.png", "resources/images/menu02.png"];
 
 pub struct Resources {
     images: HashMap<String, (Texture, f32, f32)>,
@@ -18,8 +16,10 @@ impl Resources {
     pub fn load(resource_manager: &ResourceManager) -> Self {
         let mut images = HashMap::new();
 
-        for (path, width, height) in IMAGE_PATHS {
+        for path in IMAGE_PATHS {
             let texture = block_on(resource_manager.request_texture(path)).unwrap();
+            let ImageSize { width, height } = imagesize::size(path).unwrap();
+
             images.insert(path.to_string(), (texture, width as f32, height as f32));
         }
 
