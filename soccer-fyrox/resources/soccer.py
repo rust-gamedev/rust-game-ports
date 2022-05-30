b594e0afc2437e165134cb103f97171e164c4ca7
Saverio Miroddi <saverio.pub2@gmail.com>
2022-05-30 10:19:18 +0200

    Soccer-fyrox: Implement Controls class
    Unfortunately, implementing a InputController borrow+lifetime gets ugly, due to types pollution.

--- soccer-fyrox/resources/soccer.py
+++ soccer-fyrox/resources/soccer.py
@@ -970,37 +970,37 @@ def key_just_pressed(key):
 
     return result
 
-class Controls:
-    def __init__(self, player_num):
-        if player_num == 0:
-            self.key_up = keys.UP
-            self.key_down = keys.DOWN
-            self.key_left = keys.LEFT
-            self.key_right = keys.RIGHT
-            self.key_shoot = keys.SPACE
-        else:
-            self.key_up = keys.W
-            self.key_down = keys.S
-            self.key_left = keys.A
-            self.key_right = keys.D
-            self.key_shoot = keys.LSHIFT
-
-    def move(self, speed):
-        # Return vector representing amount of movement that should occur
-        dx, dy = 0, 0
-        if keyboard[self.key_left]:
-            dx = -1
-        elif keyboard[self.key_right]:
-            dx = 1
-        if keyboard[self.key_up]:
-            dy = -1
-        elif keyboard[self.key_down]:
-            dy = 1
-        return Vector2(dx, dy) * speed
-
-    def shoot(self):
-        return key_just_pressed(self.key_shoot)
-
+# class Controls:
+#     def __init__(self, player_num):
+#         if player_num == 0:
+#             self.key_up = keys.UP
+#             self.key_down = keys.DOWN
+#             self.key_left = keys.LEFT
+#             self.key_right = keys.RIGHT
+#             self.key_shoot = keys.SPACE
+#         else:
+#             self.key_up = keys.W
+#             self.key_down = keys.S
+#             self.key_left = keys.A
+#             self.key_right = keys.D
+#             self.key_shoot = keys.LSHIFT
+#
+#     def move(self, speed):
+#         # Return vector representing amount of movement that should occur
+#         dx, dy = 0, 0
+#         if keyboard[self.key_left]:
+#             dx = -1
+#         elif keyboard[self.key_right]:
+#             dx = 1
+#         if keyboard[self.key_up]:
+#             dy = -1
+#         elif keyboard[self.key_down]:
+#             dy = 1
+#         return Vector2(dx, dy) * speed
+#
+#     def shoot(self):
+#         return key_just_pressed(self.key_shoot)
+#
 # # Pygame Zero calls the update and draw functions each frame
 #
 # class State(Enum):
