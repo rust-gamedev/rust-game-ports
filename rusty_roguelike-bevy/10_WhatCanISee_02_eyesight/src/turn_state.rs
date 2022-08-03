b18c74dbab6e81fc33905c9e048366de28c533c1
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-03 16:52:16 +0200

    Rusty Roguelike: Remove all steps except the last one (both in source and port)
    The Cargo configuration has been removed, since it doesn't serve any purpose.

--- rusty_roguelike-bevy/10_WhatCanISee_02_eyesight/src/turn_state.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum TurnState {
-    AwaitingInput,
-    PlayerTurn,
-    MonsterTurn,
-    GameOver,
-    Victory,
-}
--- source_projects/rusty_roguelike/09_WinningAndLosing_02_losing/src/turn_state.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum TurnState {
-    AwaitingInput,
-    PlayerTurn,
-    MonsterTurn,
-    GameOver,
-}
