1d3a64aa33da94512e6dbde71e4d56b61991df4e
Olle Wreede <olle@wreede.se>
2022-09-04 19:59:23 +0200

    Add Sized as supertrait to cleanup usage of 'where Self: Sized'

--- bunner-macroquad/src/active_row.rs
+++ bunner-macroquad/src/active_row.rs
@@ -1,11 +1,8 @@
 use crate::{child::Child, position::Position, WIDTH};
 use macroquad::rand;
 
-pub trait ActiveRow {
-    fn build_children(dx: i32) -> Vec<Child>
-    where
-        Self: Sized,
-    {
+pub trait ActiveRow: Sized {
+    fn build_children(dx: i32) -> Vec<Child> {
         let mut children = Vec::new();
         let mut x = -WIDTH / 2 - 70;
         while x < WIDTH / 2 + 70 {
@@ -20,22 +17,14 @@ pub trait ActiveRow {
         children
     }
 
-    fn build_child(dx: i32, position: Position) -> Child
-    where
-        Self: Sized;
+    fn build_child(dx: i32, position: Position) -> Child;
 
-    fn create_random_child(&self, dx: i32) -> Child
-    where
-        Self: Sized,
-    {
+    fn create_random_child(&self, dx: i32) -> Child {
         let pos = Position::new(if dx < 0 { WIDTH + 70 } else { -70 }, 0);
         Self::build_child(dx, pos)
     }
 
-    fn random_interval(&self, dx: i32) -> f32
-    where
-        Self: Sized,
-    {
+    fn random_interval(&self, dx: i32) -> f32 {
         // 240 is minimum distance between the start of one child object and the start of the next, assuming its
         // speed is 1. If the speed is 2, they can occur twice as frequently without risk of overlapping with
         // each other. The maximum distance is double the minimum distance (1 + random value of 1)
