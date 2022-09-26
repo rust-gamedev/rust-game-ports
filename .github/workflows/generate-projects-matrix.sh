e2034449c27775ea6e95fd17a393d8f612eacb24
Saverio Miroddi <saverio.pub2@gmail.com>
2022-09-26 20:26:26 +0200

    CI: Include only projects changed by the current PR (#147)
    CI: Run jobs only for projects that were changed

--- .github/workflows/generate-projects-matrix.sh
+++ .github/workflows/generate-projects-matrix.sh
@@ -7,22 +7,35 @@ set -o errtrace
 shopt -s inherit_errexit
 
 function main {
-  local project_dir=$1
+  local changed_dirs=$1
 
-  # Keep this simple:
-  #
-  # - jq is cool, and it's also unredable 😂;
-  # - we (can) assume that we don't need quoting;
-  # - exclude subprojects, e.g. macro crates;
+  # - we assume that we don't need quoting;
+  # - we include only the root directories, if they include a `Cargo.toml` file;
   # - the JSON is actually JSON5, since a trailing comma is present inside the array; screw JSON.
 
-  echo "["
-  find "$project_dir" -maxdepth 2 -name 'Cargo.toml' -printf '  { "port_manifest": "%P\0" },\n'
-  echo "]"
+  # We don't print newlines in order to simplify testing for empty matrices; the alternative is to
+  # use conditionals, which are a bit ugly.
+  #
+  echo -n "["
+
+  mapfile -td, changed_dirs <<< "$changed_dirs"
+
+  for changed_dir in "${changed_dirs[@]}"; do
+    if [[ $changed_dir != . && $(dirname "$changed_dir") == . ]]; then
+      local cargo_file=$changed_dir/Cargo.toml
+
+      if [[ -f $cargo_file ]]; then
+        echo "  { \"port_manifest\": \"$cargo_file\" },"
+      fi
+    fi
+  done
+
+  echo -n "]"
 }
 
-# Prints a JSON5 document with an array of `{ "port_manifest": "<relative port dir>/Cargo.toml" }`.
+# Filters in the project directories, and prints a JSON5 document with an array of
+# `{ "port_manifest": "<relative_port_dir>/Cargo.toml" }` entries.
 #
-# $1: project (workspace) directory
+# $1: list of changed dirs, separated by comma (limitation of the `changed-files` action).
 #
 main "$@"
